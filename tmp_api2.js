b7b20ae33b3f36912f5bb691789dd659b3728d20
shiguang-dev <dev@shiguang.local>
2026-09-17 15:46:16 +0800

    chore: 清理临时诊断脚本

--- tmp_api2.js
+++
@@ -1,9 +0,0 @@
-// 用 scfService 的 CLS 日志搜索
-const CloudBase = require('@cloudbase/manager-node');
-const sec = require('./cloud_secrets.json');
-const ENV = 'gerenceshi-d0gguq5u39b4b86b2';
-(async () => {
-  const app = new CloudBase({ secretId: sec.secretId, secretKey: sec.secretKey, envId: ENV, region: 'ap-shanghai' });
-  const scf = app.functions.scfService;
-  console.log('scfService keys:', Object.keys(scf).join(','));
-})().catch(e => console.error('ERR', String(e.message || e).slice(0, 200)));
